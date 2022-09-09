8fd7f7850cc05455b84d68be6edd83e2a3d38991
r21ax <r21ax@yahoo.co.jp>
2022-09-09 17:33:45 +0900

    編集画面の追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,10 +5,10 @@
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='create'>[<a href='/posts/create'>create</a>]</p>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
@@ -19,7 +19,7 @@
                 </div>
             @endforeach
         </div>
-         <div class='paginate'>
+        <div class='paginate'>
             {{ $posts->links() }}
         </div>
     </body>
