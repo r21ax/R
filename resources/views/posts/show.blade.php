8a789e58d1cbcd48eed88a1dc941bad4a44fe48e
r21ax <r21ax@yahoo.co.jp>
2022-09-10 10:22:41 +0900

    記事の論理削除追加

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,6 +12,12 @@
             {{ $post->title }}
         </h1>
         <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_delete" method="post" >
+             @csrf
+             @method('DELETE')
+             <input type="submit" style="display:none">
+             <p class='delete'>[<span onclick="return deletePost(this);">delete</span>]</p>
+        </form>
         <div class="post">
             <h2 class='title'>{{ $post->title }}</h2>
             <p class='body'>{{ $post->body }}</p>
@@ -20,5 +26,13 @@
         <div class='frooter'>
             <a href='/'>back</a>
         </div>
+        <script>
+           function deletePost(e){
+               'use strict';
+              if (confirm('本当に削除しますか？')){
+                  document.getElementById('form_delete').submit();
+               }
+            }
+        </script>
     </body>
 </html>
